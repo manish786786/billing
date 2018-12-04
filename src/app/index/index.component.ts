9ab29691d910e06b61606509ef5593f68a544e60
rashmi <rashmi@kyhatch.com>
2018-12-04 18:12:22 +0530

    routing ui

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -12,4 +12,17 @@ export class IndexComponent implements OnInit {
   ngOnInit() {
   }
 
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+  
 }
