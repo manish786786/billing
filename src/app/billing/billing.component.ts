9eaacb7f00739e9909d1f7fa361dcea560c36a8e
rashmi <rashmi@kyhatch.com>
2018-12-16 18:36:17 +0530

    bill ui

--- src/app/billing/billing.component.ts
+++ src/app/billing/billing.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-billing',
   templateUrl: './billing.component.html',
-  styleUrls: ['./billing.component.css']
+  styleUrls: ['./billing.component.scss']
 })
 export class BillingComponent implements OnInit {
+  currentDate: Date;
+ 
 
   constructor() { }
 
   ngOnInit() {
+    this.currentDate = new Date();
   }
+
 
 }
